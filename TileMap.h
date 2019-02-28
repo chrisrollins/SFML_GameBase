f8f21e3e17e138d18efcb3ae6e636acea94e0214
Leia Lin <astrob3rry@gmail.com>
2019-02-27 21:50:11 -0800

    make the tiles accessible

--- TileMap.h
+++ TileMap.h
@@ -4,89 +4,96 @@
 #include "SFML/Graphics.hpp"
 
 namespace Engine {
-class TileMap : public sf::Drawable, public sf::Transformable
-{
-public:
-
-	bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+	class TileMap : public sf::Drawable, public sf::Transformable
 	{
-		// load the tileset texture
-		if (!m_tileset.loadFromFile(tileset))
-			return false;
-		
-		this->_tileSize = tileSize;
-		this->_width = width;
-		this->_height = height;
+	public:
 
-		// resize the vertex array to fit the level size
-		m_vertices.setPrimitiveType(sf::Quads);
-		m_vertices.resize(width * height * 4);
+		bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+		{
+			// load the tileset texture
+			if (!m_tileset.loadFromFile(tileset))
+				return false;
 
-		// populate the vertex array, with one quad per tile
-		for (unsigned int i = 0; i < width; ++i)
-			for (unsigned int j = 0; j < height; ++j)
-			{
-				// get the current tile number
-				int tileNumber = tiles[i + j * width];
+			this->_tileSize = tileSize;
+			this->_width = width;
+			this->_height = height;
+			this->tiles = tiles;
 
-				// find its position in the tileset texture
-				int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-				int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+			// resize the vertex array to fit the level size
+			m_vertices.setPrimitiveType(sf::Quads);
+			m_vertices.resize(width * height * 4);
 
-				// get a pointer to the current tile's quad
-				sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+			// populate the vertex array, with one quad per tile
+			for (unsigned int i = 0; i < width; ++i)
+				for (unsigned int j = 0; j < height; ++j)
+				{
+					// get the current tile number
+					int tileNumber = tiles[i + j * width];
 
-				// define its 4 corners
-				quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-				quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-				quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-				quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+					// find its position in the tileset texture
+					int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+					int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
 
-				// define its 4 texture coordinates
-				quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
-				quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-				quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-				quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-			}
+					// get a pointer to the current tile's quad
+					sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
 
-		return true;
-	}
+					// define its 4 corners
+					quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+					quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+					quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+					quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
 
-	unsigned int width() const
-	{
-		return this->_width;
-	}
+					// define its 4 texture coordinates
+					quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+					quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+					quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+					quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+				}
 
-	unsigned int height() const
-	{
-		return this->_height;
-	}
+			return true;
+		}
 
-	sf::Vector2u tileSize() const
-	{
-		return this->_tileSize;
-	}
+		unsigned int width() const
+		{
+			return this->_width;
+		}
 
-private:
+		unsigned int height() const
+		{
+			return this->_height;
+		}
 
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-	{
-		// apply the transform
-		states.transform *= getTransform();
+		sf::Vector2u tileSize() const
+		{
+			return this->_tileSize;
+		}
 
-		// apply the tileset texture
-		states.texture = &m_tileset;
+		int getTileAt(int i, int j) const
+		{
+			return tiles[i + j * this->width()];
+		}
 
-		// draw the vertex array
-		target.draw(m_vertices, states);
-	}
+	private:
 
-	sf::Vector2u _tileSize;
-	unsigned int _width;
-	unsigned int _height;
-	sf::VertexArray m_vertices;
-	sf::Texture m_tileset;
-};
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+		{
+			// apply the transform
+			states.transform *= getTransform();
+
+			// apply the tileset texture
+			states.texture = &m_tileset;
+
+			// draw the vertex array
+			target.draw(m_vertices, states);
+		}
+
+		sf::Vector2u _tileSize;
+		unsigned int _width;
+		unsigned int _height;
+		const int* tiles;
+		sf::VertexArray m_vertices;
+		sf::Texture m_tileset;
+	};
 }
 
 #endif
