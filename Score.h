3c09b2dd6071b49a57a95b9119f9e2da747cedd0
JakinHuangWang <darkfact247@gmail.com>
2019-02-28 17:44:58 -0800

    Add Score Incrementation on top right hand side

--- Score.h
+++ Score.h
@@ -22,12 +22,18 @@ public:
     Score(std::string text, const sf::Color &C, unsigned int size,
           float position_x, float position_y) : SampleUIText(text, C, size, position_x, position_y) {}
     Score(int s, const sf::Color &C, unsigned int size,
-          float position_x, float position_y) : number(s), SampleUIText(std::to_string(s), C, size, position_x, position_y) {}
+          float position_x, float position_y) : number(s), SampleUIText("Your Score: " + std::to_string(s), C, size, position_x, position_y) {}
     void EveryFrame(uint64_t f){
-        if(f % 60  == 0){
+        if(f % 5  == 0){
             number++;
-            this->setText(std::to_string(number));
+            this->setText("Your Score: " + std::to_string(number));
         }
+        int n = number, count = 0;
+        while(n){
+            count++;
+            n/= 10;
+        }
+        this->setPosition(1000 - (count + 11) * 16, 0);
     }
 private:
     int number = 0;
