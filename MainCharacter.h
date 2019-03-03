f7445fa772a544bc4750f1729684cf38a9e74803
Leia Lin <astrob3rry@gmail.com>
2019-03-02 16:38:10 -0800

    edit main character's animation
    change the sprite sheet of the main character,
    add melee attack animation + death animation(credit to Erik's artwork)

--- MainCharacter.h
+++ MainCharacter.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_CHARACTER_HEADER
 #define MAIN_CHARACTER_HEADER
 
+#include <cmath>
 #include "Screen.h"
 #include "GameObject.h"
 #include "SkeletonBlast.h"
 #include "Citizen.h"
+#include "Bullet.h"
+#include "Soldier.h"
 
 using namespace Engine;
 
@@ -49,14 +52,24 @@ class MainCharacter : public GraphicalGameObject
 	sf::Vector2u textureSize;
 	sf::Vector2u imageCount;
 	sf::Vector2u currentImage;
-    int _health = 30*60; // 60 frames per 60 seconds
-    int maxHealth = 30*60;
+	bool meleeAttack;
+	bool isWalking; // used so that the zombie stands still when the key is not pressed
+	int deathCount; // to control death animation
+	bool isDead; // true when the zombie turns to invisible
+	DIRECTION direction;
+	int _health = 30 * 60; // 60 frames per second
+	int maxHealth = 30 * 60;
+	int baseSpeed = 2;
+	int speed = 2;
+	int maxSpeed = 5;
+	int speedDecayDelay = 0;
+	int speedRestoreDelay = 0;
 public:
 	MainCharacter(sf::Sprite s) : GraphicalGameObject(s)
 	{
 		textureSize = this->sprite()->getTexture()->getSize();
-		textureSize.x /= 3;
-		textureSize.y /= 4;
+		textureSize.x /= 4;
+		textureSize.y /= 12;
 		imageCount.x = 0;
 		this->sprite()->setTextureRect(sf::IntRect(imageCount.x * textureSize.x,
 			imageCount.y * textureSize.y, textureSize.x, textureSize.y));
@@ -67,9 +80,14 @@ public:
 		this->obstacleCollisionSize.height = size.height * collisionSizeRatio.y;
 		this->obstacleCollisionSize.left = ((1 - collisionSizeRatio.x) * size.width) / 2;
 		this->obstacleCollisionSize.top = ((1 - collisionSizeRatio.y) * size.height);
+		meleeAttack = false;
+		isWalking = false;
+		deathCount = 0;
+		isDead = false;
 	}
 	void KeyPressed(sf::Event e)
 	{
+		isWalking = true;
 		switch (e.key.code)
 		{
 		case sf::Keyboard::W:
@@ -90,19 +108,24 @@ public:
 	}
 	void KeyReleased(sf::Event e)
 	{
+		isWalking = false;
 		switch (e.key.code)
 		{
 		case sf::Keyboard::W:
 			this->W_KeyHeld = false;
+			direction = DIRECTION::UP;
 			break;
 		case sf::Keyboard::A:
 			this->A_KeyHeld = false;
+			direction = DIRECTION::LEFT;
 			break;
 		case sf::Keyboard::S:
 			this->S_KeyHeld = false;
+			direction = DIRECTION::DOWN;
 			break;
 		case sf::Keyboard::D:
 			this->D_KeyHeld = false;
+			direction = DIRECTION::RIGHT;
 			break;
 		default:
 			break;
@@ -110,86 +133,206 @@ public:
 	}
 	void MouseButtonReleased(sf::Event e)
 	{
-		sf::Vector2i mousePos = this->screen->getMousePosition();
-		sf::Vector2f distance = static_cast<sf::Vector2f>(mousePos) - this->sprite()->getPosition();
-		sf::Vector2f shotOrigin = this->sprite()->getPosition();
-		sf::IntRect size = this->sprite()->getTextureRect();
-		shotOrigin.x += size.width / 2;
-		shotOrigin.y += size.height / 4;
-		SkeletonBlast* blast = new SkeletonBlast(sf::Sprite(blast_texture), shotOrigin, sf::Vector2f(mousePos.x, mousePos.y));
-		this->screen->add(blast);
+		if (_health > 0)
+		{
+			if (e.mouseButton.button == sf::Mouse::Left)
+			{
+				meleeAttack = true;
+				isWalking = false;
+				if (direction == DIRECTION::UP)
+				{
+					imageCount.y = 8;
+					imageCount.x = 2;
+				}
+				if (direction == DIRECTION::DOWN)
+				{
+					imageCount.y = 8;
+					imageCount.x = 0;
+				}
+				if (direction == DIRECTION::LEFT)
+				{
+					imageCount.y = 9;
+					imageCount.x = 2;
+				}
+				if (direction == DIRECTION::RIGHT)
+				{
+					imageCount.y = 9;
+					imageCount.x = 0;
+				}
+				this->sprite()->setTextureRect(sf::IntRect(imageCount.x * textureSize.x,
+					imageCount.y * textureSize.y, textureSize.x, textureSize.y));
+			}
+			else
+			{
+				isWalking = false;
+				sf::Vector2i mousePos = this->screen->getMousePosition();
+				sf::Vector2f distance = static_cast<sf::Vector2f>(mousePos) - this->sprite()->getPosition();
+				sf::Vector2f shotOrigin = this->sprite()->getPosition();
+				sf::IntRect size = this->sprite()->getTextureRect();
+				shotOrigin.x += size.width / 2;
+				shotOrigin.y += size.height / 4;
+				SkeletonBlast* blast = new SkeletonBlast(sf::Sprite(blast_texture), shotOrigin, sf::Vector2f(mousePos.x, mousePos.y));
+				this->screen->add(blast);
+			}
+		}
 	}
 	void EveryFrame(uint64_t f)
 	{
 		sf::Sprite* s = this->sprite();
-		if (f % 20 == 0)
+		if (_health > 0)
 		{
-			if (imageCount.x == 2)
-				imageCount.x = 0;
-			else
-				imageCount.x++;
+			if (f % 30 == 0 && meleeAttack)
+			{
+				if (direction == DIRECTION::DOWN || direction == DIRECTION::RIGHT)
+				{
+					if (imageCount.x == 1)
+					{
+						meleeAttack = false;
+						if (direction == DIRECTION::DOWN)
+							imageCount.y = 0;
+						else
+							imageCount.y = 2;
+					}
+					else
+						imageCount.x++;
+				}
+				else //if (direction == DIRECTION::UP || direction == DIRECTION::LEFT)
+				{
+					if (imageCount.x == 3)
+					{
+						meleeAttack = false;
+						if (direction == DIRECTION::UP)
+							imageCount.y = 3;
+						else
+							imageCount.y = 1;
+					}
+					else
+						imageCount.x++;
+				}
+			}
+			if (!meleeAttack && isWalking)
+			{
+				if (f % 20 == 0)
+				{
+					if (imageCount.x == 3)
+						imageCount.x = 0;
+					else
+						imageCount.x++;
+				}
+				if (this->W_KeyHeld)
+				{
+					imageCount.y = 3;
+					s->move(0, -1 * this->speed);
+				}
+				if (this->A_KeyHeld)
+				{
+					imageCount.y = 1;
+					s->move(-1 * this->speed, 0);
+				}
+				if (this->S_KeyHeld)
+				{
+					imageCount.y = 0;
+					s->move(0, this->speed);
+				}
+				if (this->D_KeyHeld)
+				{
+					imageCount.y = 2;
+					s->move(this->speed, 0);
+				}
+			}
+			_health--;
+			//speed goes back to base speed gradually
+			if (f % 6 == 0)
+			{
+				if (this->speed > this->baseSpeed)
+				{
+					speedRestoreDelay = 0;
+					if (this->speedDecayDelay > 0) { this->speedDecayDelay--; }
+					else { this->changeSpeed(-1); }
+				}
+				else if (this->speed < this->baseSpeed)
+				{
+					speedDecayDelay = 0;
+					if (this->speedRestoreDelay > 0) { this->speedRestoreDelay--; }
+					else { this->changeSpeed(1); }
+				}
+			}
 		}
-		if (this->W_KeyHeld)
+		else
 		{
-			imageCount.y = 3;
+			imageCount.x = deathCount;
+			if (f % 20 == 0 && !isDead)
+			{
+				deathCount++;
+			}
+			if (direction == DIRECTION::DOWN || S_KeyHeld)
+			{
+				imageCount.y = 4;
+			}
+			if (direction == DIRECTION::LEFT || A_KeyHeld)
+			{
+				imageCount.y = 5;
+			}
+			if (direction == DIRECTION::RIGHT || D_KeyHeld)
+			{
+				imageCount.y = 6;
+			}
+			if (direction == DIRECTION::UP || W_KeyHeld)
+			{
+				imageCount.y = 7;
+			}
+			if (imageCount.x == 3)
+			{
+				this->sprite()->setColor(sf::Color(0, 0, 0, 0));
+				isDead = true;
+			}
+		}
+		if (!isDead)
+		{
 			this->sprite()->setTextureRect(sf::IntRect(imageCount.x * textureSize.x,
 				imageCount.y * textureSize.y, textureSize.x, textureSize.y));
-			s->move(0, -2);
 		}
-		if (this->A_KeyHeld)
-		{
-			imageCount.y = 1;
-			this->sprite()->setTextureRect(sf::IntRect(imageCount.x * textureSize.x,
-				imageCount.y * textureSize.y, textureSize.x, textureSize.y));
-			s->move(-2, 0);
-		}
-		if (this->S_KeyHeld)
-		{
-			imageCount.y = 0;
-			this->sprite()->setTextureRect(sf::IntRect(imageCount.x * textureSize.x,
-				imageCount.y * textureSize.y, textureSize.x, textureSize.y));
-			s->move(0, 2);
-		}
-		if (this->D_KeyHeld)
-		{
-			imageCount.y = 2;
-			this->sprite()->setTextureRect(sf::IntRect(imageCount.x * textureSize.x,
-				imageCount.y * textureSize.y, textureSize.x, textureSize.y));
-			s->move(2, 0);
-		}
-        if (_health > 0)
-            _health--;
 	}
-    int getHealth(){
-        return _health;
-    }
-    int getMaxHealth(){
-        return maxHealth;}
-    
+	int getHealth() {
+		return _health;
+	}
+	int getMaxHealth() {
+		return maxHealth;
+	}
+	void changeHealth(int change)
+	{
+		this->_health += change;
+		if (this->_health > this->maxHealth) { this->_health = this->maxHealth; }
+	}
+	void changeSpeed(int change)
+	{
+		this->speed += change;
+		if (this->speed > this->maxSpeed) { this->speed = this->maxSpeed; }
+		else if (this->speed < 1) { this->speed = 1; }
+	}
 	void Collision(GraphicalGameObject& other)
 	{
-        if (_health>0){
-            if (dynamic_cast<Bullet*>(&other))
-            {
-                _health-=10;
-            }
-            else if (dynamic_cast<Soldier*>(&other))
-            {
-                _health-=10;
-            }
-            else if (dynamic_cast<SkeletonBlast*>(&other)){
-                
-            }
-            else //if (dynamic_cast<Citizen*>(&other))
-            {
-                //this->screen->remove(&other);
-                if(_health<=maxHealth-20)
-                _health+=20;
-            }
-        
-        }
-        // if collision happened:
-        //_health -= other.getDamage();  // need to add the damage to graphicak game object?
+		if (_health > 0) {
+			if (dynamic_cast<Bullet*>(&other))
+			{
+				this->changeHealth(-5);
+			}
+			else if (dynamic_cast<Soldier*>(&other))
+			{
+				this->changeHealth(-10);
+				this->changeSpeed(-1);
+			}
+			else if (dynamic_cast<Citizen*>(&other))
+			{
+				this->screen->remove(&other);
+				float missingHealthMultiplier = 2.0f - (this->_health / this->maxHealth);
+				this->changeHealth(60 * missingHealthMultiplier);
+				this->changeSpeed(1);
+				this->speedDecayDelay = 60;
+			}
+		}
+		// if collision happened:
+		//_health -= other.getDamage();  // need to add the damage to graphicak game object?
 		//std::cout << "collision in the main character" << std::endl;
 		//std::cout << " " << std::endl;
 		//std::cout << " " << std::endl;
