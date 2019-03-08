522c7888f365297c3196dd28133142fa8b208679
Leia Lin <astrob3rry@gmail.com>
2019-03-07 17:40:13 -0800

    added a volume parameter

--- SoundPlayer.h
+++ SoundPlayer.h
@@ -1,5 +1,5 @@
-#ifndef SoundPlayer_h
-#define SoundPlayer_h
+#ifndef SOUNDPLAYER_H
+#define SOUNDPLAYER_H
 
 #include "SFML/Audio.hpp"
 #include <list>
@@ -14,7 +14,6 @@ namespace SoundEffect
 		ZombieAttack,
 		ZombieGroan,
 		ZombieDeath,
-		MageAttack,
 		MageDeath,
 		Alarm,
 		MenuClick
@@ -26,61 +25,50 @@ class SoundHolder
 private:
 	std::map<SoundEffect::ID, std::unique_ptr<sf::SoundBuffer>> mSoundMap;
 public:
-	void loadSoundEffect(SoundEffect::ID id, const std::string& filename);
-	sf::SoundBuffer& getSoundBuffer(SoundEffect::ID id);
+	void loadSoundEffect(SoundEffect::ID id, const std::string& filename)
+	{
+		std::unique_ptr<sf::SoundBuffer> sound(new sf::SoundBuffer);
+		sound->loadFromFile(filename);
+		mSoundMap.insert(std::make_pair(id, std::move(sound)));
+	}
+	sf::SoundBuffer& getSoundBuffer(SoundEffect::ID id)
+	{
+		auto found = mSoundMap.find(id);
+		return *(found->second);
+	}
 };
-
-void SoundHolder::loadSoundEffect(SoundEffect::ID id, const std::string& filename)
-{
-	std::unique_ptr<sf::SoundBuffer> sound(new sf::SoundBuffer);
-	sound->loadFromFile(filename);
-	mSoundMap.insert(std::make_pair(id, std::move(sound)));
-}
-
-sf::SoundBuffer& SoundHolder::getSoundBuffer(SoundEffect::ID id)
-{
-	auto found = mSoundMap.find(id);
-	return *(found->second);
-}
 
 class SoundPlayer : private sf::NonCopyable
 {
 public:
-	SoundPlayer();
-	void play(SoundEffect::ID effect);
-	void removeStoppedSounds();
+	SoundPlayer() :mSoundContainer(), mSounds()
+	{
+		mSoundContainer.loadSoundEffect(SoundEffect::ZombieEat1, "zombie_eat1.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::ZombieEat2, "zombie_eat2.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::ZombieEat3, "zombie_eat3.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::ZombieAttack, "zombie_attack.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::ZombieGroan, "zombie_hurt.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::ZombieDeath, "zombie_death.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::MageDeath, "mage_death.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::Alarm, "alarm.ogg");
+		mSoundContainer.loadSoundEffect(SoundEffect::MenuClick, "menu_buttonclick.ogg");
+	}
+	void play(SoundEffect::ID effect, float volume)
+	{
+		mSounds.push_back(sf::Sound(mSoundContainer.getSoundBuffer(effect)));
+		mSounds.back().setVolume(volume);
+		mSounds.back().play();
+	}
+	void removeStoppedSounds()
+	{
+		mSounds.remove_if([](const sf::Sound& s)
+		{
+			return s.getStatus() == sf::Sound::Stopped;
+		});
+	}
 
 private:
 	SoundHolder mSoundContainer;
 	std::list<sf::Sound> mSounds;
 };
-
-SoundPlayer::SoundPlayer() :mSoundContainer(), mSounds()
-{
-	mSoundContainer.loadSoundEffect(SoundEffect::ZombieEat1, "zombie_eat1.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::ZombieEat2, "zombie_eat2.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::ZombieEat3, "zombie_eat3.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::ZombieAttack, "zombie_attack.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::ZombieGroan, "zombie_hurt.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::ZombieDeath, "zombie.death.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::MageAttack, "mage_attack.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::MageDeath, "mage_death.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::Alarm, "alarm.ogg");
-	mSoundContainer.loadSoundEffect(SoundEffect::MenuClick, "menu_buttonclick.ogg");
-}
-
-void SoundPlayer::play(SoundEffect::ID effect)
-{
-	mSounds.push_back(sf::Sound(mSoundContainer.getSoundBuffer(effect)));
-	mSounds.back().play();
-}
-
-void SoundPlayer::removeStoppedSounds()
-{
-	mSounds.remove_if([](const sf::Sound& s)
-	{
-		return s.getStatus() == sf::Sound::Stopped;
-	});
-}
-
-#endif /* SoundPlayer_h */
+#endif
