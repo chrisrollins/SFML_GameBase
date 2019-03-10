6e3f508a9f212467377fad629466636bb8b3f08d
Leia Lin <astrob3rry@gmail.com>
2019-03-10 12:16:47 -0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -2,10 +2,33 @@
 #include "TestLevel.h"
 #include "DifficultySettings.h"
 
+class Name : public GraphicalGameObject {
+public:
+	Name() : GraphicalGameObject(sf::Text())
+	{
+		font.loadFromFile("DoubleFeature.ttf");
+		this->textPtr()->setFont(font);
+		this->textPtr()->setString("Cursed Zombie");
+		this->textPtr()->Bold;
+		this->textPtr()->setFillColor(sf::Color(179, 45, 0));
+		this->textPtr()->setCharacterSize(50);
+		this->textPtr()->setPosition(340.f, 50.f);
+	}
+private:
+	sf::Font font;
+	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->getGraphic()); }
+};
+
 class EasyLevelButton : public UIButton
 {
 public:
-	EasyLevelButton() : UIButton("Easy", { 412.f, 162.f }, { 200.f, 75.f }) { }
+	EasyLevelButton() : UIButton("Easy", { 412.f, 162.f }, { 200.f, 75.f }) 
+	{ 
+		this->setFont("doublefeature.ttf");
+		this->setTextSize(35.f);
+		this->setTextColor(sf::Color(179, 45, 0));
+		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+	}
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
@@ -22,7 +45,13 @@ public:
 
 class NormalLevelButton : public UIButton {
 public:
-	NormalLevelButton() : UIButton("Normal", { 412.f, 242.f }, { 200.f, 75.f }) {}
+	NormalLevelButton() : UIButton("Normal", { 412.f, 242.f }, { 200.f, 75.f }) 
+	{
+		this->setFont("doublefeature.ttf");
+		this->setTextSize(35.f);
+		this->setTextColor(sf::Color(179, 45, 0));
+		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+	}
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
@@ -40,7 +69,13 @@ public:
 
 class HardLevelButton : public UIButton {
 public:
-	HardLevelButton() : UIButton("Insane", { 412.f, 322.f }, { 200.f, 75.f }) {}
+	HardLevelButton() : UIButton("Insane", { 412.f, 322.f }, { 200.f, 75.f }) 
+	{
+		this->setFont("doublefeature.ttf");
+		this->setTextSize(35.f);
+		this->setTextColor(sf::Color(179, 45, 0));
+		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+	}
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
@@ -56,27 +91,52 @@ public:
 	}
 };
 
-class Name : public GraphicalGameObject {
+class TutorialButton : public UIButton {
 public:
-	Name() : GraphicalGameObject(sf::Text()) 
+	TutorialButton() : UIButton("Tutorial", { 412.f, 402.f }, { 200.f, 75.f })
 	{
-		font.loadFromFile("DoubleFeature.ttf");
-		this->textPtr()->setFont(font);
-		this->textPtr()->setString("Cursed Zombie");
-		this->textPtr()->Bold;
-		this->textPtr()->setFillColor(sf::Color(179, 45, 0));
-		this->textPtr()->setCharacterSize(50);
-		this->textPtr()->setPosition(340.f, 50.f);
+		this->setFont("doublefeature.ttf");
+		this->setTextSize(35.f);
+		this->setTextColor(sf::Color(179, 45, 0));
+		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
 	}
-private:
-	sf::Font font;
-	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->getGraphic()); }
+	void MouseButtonReleased(sf::Event e)
+	{
+		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+		{
+		}
+	}
+};
+
+class ScoreboardButton : public UIButton {
+public:
+	ScoreboardButton() : UIButton("Scoreboard", { 412.f, 482.f }, { 200.f, 75.f })
+	{
+		this->setFont("doublefeature.ttf");
+		this->setTextSize(35.f);
+		this->setTextColor(sf::Color(179, 45, 0));
+		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+	}
+	void MouseButtonReleased(sf::Event e)
+	{
+		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+		{
+		}
+	}
 };
 
 class QuitButton : public UIButton
 {
 public:
-	QuitButton() : UIButton("Quit", { 412.f, 602.f }, { 200.f, 75.f }) { }
+	QuitButton() : UIButton("Escape", { 412.f, 562.f }, { 200.f, 75.f }) 
+	{
+		this->setFont("doublefeature.ttf");
+		this->setTextSize(35.f);
+		this->setTextColor(sf::Color(179, 45, 0));
+		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+	}
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
@@ -97,7 +157,9 @@ namespace Engine
 			new Name(),
 			new EasyLevelButton(),
 			new NormalLevelButton(),
-			new HardLevelButton,
+			new HardLevelButton(),
+			new TutorialButton(),
+			new ScoreboardButton(),
 			new QuitButton()
 		};
 	}
