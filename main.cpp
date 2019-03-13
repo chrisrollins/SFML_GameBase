d8439f05a9304b248315fb535ffceef7cca60e71
Leia Lin <astrob3rry@gmail.com>
2019-03-13 10:23:12 -0700

    cleaned the code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include "Screen.h"
 #include "Menu.h"
-#include "ScoreBoard.h"
 
 using namespace Engine;
 
 int main(int argc, char** argv)
 {
-	//these are static members that should be set before rendering a screen. the window will be locked at these values after rendering has started.
+	// these are static members that should be set before rendering a screen. 
+	// the window will be locked at these values after rendering has started.
 	Screen::windowWidth = 1024;
 	Screen::windowHeight = 768;
-	Screen::windowTitle = "Game!";
-	
+	Screen::windowTitle = "Cursed Zombie";
+
 	Menu* menu = new Menu();
 	menu->start();
 
