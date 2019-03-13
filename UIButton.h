a19751461fccd8713cff12d01771ce7d460b7811
Leia Lin <astrob3rry@gmail.com>
2019-03-13 10:26:37 -0700

    cleaned the code

--- UIButton.h
+++ UIButton.h
@@ -1,5 +1,5 @@
-#ifndef UI_BUTTON_HEADER
-#define UI_BUTTON_HEADER
+#ifndef UIBUTTON_H
+#define UIBUTTON_H
 
 #include "GameObject.h"
 #include <iostream>
@@ -8,23 +8,30 @@ using namespace Engine;
 
 class UIButton : public GraphicalGameObject
 {
+protected:
+	sf::Vector2f position;
+	sf::Font myFont;
+	sf::RectangleShape background;
+	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->getGraphic()); }
 public:
 	UIButton(std::string text, sf::Vector2f position, sf::Vector2f size) : GraphicalGameObject(sf::Text())
 	{
-		if (myFont.loadFromFile("arial.ttf"))
+		if (this->myFont.loadFromFile("arial.ttf"))
 		{
-			this->textPtr()->setFont(myFont);
+			this->textPtr()->setFont(this->myFont);
 			this->textPtr()->setString(text);
 		}
-		this->background.setFillColor(sf::Color(70, 70, 70));
+		this->background.setFillColor({ 70, 70, 70 });
 		this->background.setSize(size);
 		this->setPosition(position);
 	}
+
 	void setFont(std::string font)
 	{
-		myFont.loadFromFile(font);
-		this->textPtr()->setFont(myFont);
+		this->myFont.loadFromFile(font);
+		this->textPtr()->setFont(this->myFont);
 	}
+
 	void setPosition(sf::Vector2f position)
 	{
 		this->position = position;
@@ -38,28 +45,27 @@ public:
 			this->textPtr()->setPosition(position + textPositionAdjust);
 		}
 	}
+
 	void setTextSize(float size)
 	{
 		this->textPtr()->setCharacterSize(static_cast<unsigned int>(size));
 	}
+
 	void setTextColor(sf::Color color)
 	{
 		this->textPtr()->setFillColor(color);
 	}
+
 	void setBackgroundColor(sf::Color color)
 	{
 		this->background.setFillColor(color);
 	}
+
 	void draw(sf::RenderWindow& win)
 	{
 		win.draw(this->background);
 		win.draw(*this->graphic);
 	}
-protected:
-	sf::Vector2f position;
-	sf::Font myFont;
-	sf::RectangleShape background;
-	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->getGraphic()); }
 };
 
 #endif
