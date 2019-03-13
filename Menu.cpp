4995834bfe9e9a85080681bafa3f49823bc09a11
Leia Lin <astrob3rry@gmail.com>
2019-03-13 12:19:37 -0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -19,7 +19,7 @@ public:
 		this->font.loadFromFile("Lycanthrope.ttf");
 		this->textPtr()->setFont(this->font);
 		this->textPtr()->setStyle(sf::Text::Bold);
-		this->textPtr()->setFillColor(sf::Color(179, 45, 0));
+		this->textPtr()->setFillColor({ 179, 45, 0 });
 		this->textPtr()->setCharacterSize(50U);
 		if (DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST)
 		{
@@ -29,7 +29,7 @@ public:
 		else
 		{
 			this->textPtr()->setPosition(100.f, 80.f);
-			this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n");
+			this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but the \nguardian requires a password:\n");
 		}
 		this->background.setPosition(600.f, 400.f);
 		this->texture.loadFromFile("bloodyhands.png");
@@ -54,7 +54,7 @@ public:
 			do
 			{
 				;
-			} while (this->clock.getElapsedTime().asSeconds() < 1.5f); // pause the good luck msg for some time before the real game start
+			} while (this->clock.getElapsedTime().asSeconds() < 1.5f); // pause the 'good luck' msg for some time before the real game start
 			{
 				switch (DifficultySettings::currentDifficulty)
 				{
@@ -143,24 +143,47 @@ public:
 		{
 			this->name += static_cast<char>(e.text.unicode);
 			if (DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST) { this->textPtr()->setString("Enter your name while you can:\n" + this->name); }
-			else { this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n" + this->name); }
+			else { this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but the \nguardian requires a password:\n" + this->name); }
 		}
 	}
 };
 
-class TestModeButton : public UIButton
+class TestModeButton : public GraphicalGameObject
 {
 private:
+	sf::Vector2u textureSize;
+	sf::Vector2u imageCount;
+	sf::Vector2u currentImage;
 	bool activated = false;
 	bool enabled = true;
 	bool enterPressed = false;
+	sf::Color color;
+	sf::Texture guardianTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	TestModeButton() : UIButton("?", { 200.f, 25.f }, { 100.f, 100.f })
+	TestModeButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->myFont.loadFromFile("harting.ttf");
-		this->textPtr()->setFont(myFont);
-		this->textPtr()->setCharacterSize(50);
-		this->textPtr()->setFillColor({ 179, 45, 0 });
+		this->guardianTexture.loadFromFile("guardian.png");
+		this->guardianTexture.setSmooth(true);
+		this->spritePtr()->setTexture(this->guardianTexture);
+		this->textureSize = this->spritePtr()->getTexture()->getSize();
+		this->textureSize.x /= 3;
+		this->imageCount.x = 0;
+		this->spritePtr()->setPosition(320.f, 160.f);
+		this->color = this->spritePtr()->getColor();
+		this->spritePtr()->setColor({ 0, 0, 0, 0 });
+	}
+
+	void EveryFrame(uint64_t f)
+	{
+		if (f % 15 == 0)
+		{
+			if (this->imageCount.x == 2) { this->imageCount.x = 0; }
+			else { this->imageCount.x++; }
+		}
+
+		this->spritePtr()->setTextureRect(sf::IntRect(this->imageCount.x * this->textureSize.x,
+			this->imageCount.y * this->textureSize.y, this->textureSize.x, this->textureSize.y));
 	}
 
 	void KeyReleased(sf::Event e)
@@ -168,7 +191,11 @@ public:
 		if (e.key.code == sf::Keyboard::Enter)
 		{
 			this->enterPressed = true;
-			if (this->enterPressed && this->enabled) { this->activated = true; }
+			if (this->enterPressed && this->enabled) 
+			{ 
+				this->activated = true; 
+				this->spritePtr()->setColor(this->color);
+			}
 		}
 	}
 
@@ -176,17 +203,12 @@ public:
 	{
 		if (!this->enabled || !this->activated) { return; }
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::TEST);
 			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
 			this->screen->addUIObject(new PlayerNameEntry());
 		}
-	}
-
-	void draw(sf::RenderWindow& win)
-	{
-		if (this->activated && this->enabled) { win.draw(*this->graphic); }
 	}
 
 	void enable()
@@ -209,27 +231,27 @@ public:
 	MenuBackground() : GraphicalGameObject(sf::Sprite())
 	{
 		this->backgroundTexture.loadFromFile("menu_background.png");
-		this->spritePtr()->setTexture(backgroundTexture);
+		this->spritePtr()->setTexture(this->backgroundTexture);
 	}
 };
 
-class EasyLevelButton : public UIButton
+class EasyLevelButton : public GraphicalGameObject
 {
+private:
+	sf::Texture easyButtonTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	EasyLevelButton() : UIButton("Easy", { 372.f, 162.f }, { 250.f, 75.f })
+	EasyLevelButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->setFont("Lycanthrope.ttf");
-		this->setTextSize(50.f);
-		this->setTextColor({ 179, 45, 0 });
-		this->setBackgroundColor(sf::Color::White);
-		//this->setPosition({ 372.f, 162.f });
-		//this->setBackgroundColor({ 0, 0, 0, 0 });
+		this->easyButtonTexture.loadFromFile("menu_easy.png");
+		this->spritePtr()->setTexture(this->easyButtonTexture);
+		this->spritePtr()->setPosition(440.f, 170.f);
 	}
 
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::EASY);
 			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
@@ -238,21 +260,23 @@ public:
 	}
 };
 
-class NormalLevelButton : public UIButton {
+class NormalLevelButton : public GraphicalGameObject
+{
+private:
+	sf::Texture normalButtonTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	NormalLevelButton() : UIButton("Normal", { 372.f, 242.f }, { 250.f, 75.f })
+	NormalLevelButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->setFont("Lycanthrope.ttf");
-		this->setTextSize(50.f);
-		this->setTextColor({ 179, 45, 0 });
-		this->setBackgroundColor(sf::Color::White);
-		//this->setBackgroundColor({ 0, 0, 0, 0 });
+		this->normalButtonTexture.loadFromFile("menu_normal.png");
+		this->spritePtr()->setTexture(this->normalButtonTexture);
+		this->spritePtr()->setPosition(400.f, 260.f);
 	}
 
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::NORMAL);
 			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
@@ -261,21 +285,23 @@ public:
 	}
 };
 
-class HardLevelButton : public UIButton {
+class HardLevelButton : public GraphicalGameObject
+{
+private:
+	sf::Texture hardButtonTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	HardLevelButton() : UIButton("Insane", { 372.f, 322.f }, { 250.f, 75.f })
+	HardLevelButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->setFont("Lycanthrope.ttf");
-		this->setTextSize(50.f);
-		this->setTextColor({ 179, 45, 0 });
-		this->setBackgroundColor(sf::Color::White);
-		//this->setBackgroundColor({ 0, 0, 0, 0 });
+		this->hardButtonTexture.loadFromFile("menu_insane.png");
+		this->spritePtr()->setTexture(this->hardButtonTexture);
+		this->spritePtr()->setPosition(420.f, 360.f);
 	}
 
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::HARD);
 			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
@@ -284,21 +310,23 @@ public:
 	}
 };
 
-class TutorialButton : public UIButton {
+class TutorialButton : public GraphicalGameObject
+{
+private:
+	sf::Texture tutorialButtonTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	TutorialButton() : UIButton("Tutorial", { 372.f, 402.f }, { 250.f, 75.f })
+	TutorialButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->setFont("Lycanthrope.ttf");
-		this->setTextSize(50.f);
-		this->setTextColor({ 179, 45, 0 });
-		this->setBackgroundColor(sf::Color::White);
-		//this->setBackgroundColor({ 0, 0, 0, 0 });
+		this->tutorialButtonTexture.loadFromFile("menu_tutorial.png");
+		this->spritePtr()->setTexture(this->tutorialButtonTexture);
+		this->spritePtr()->setPosition(390.f, 450.f);
 	}
 
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			Tutorial* tutorial = new Tutorial();
 			this->screen->addUIObject(tutorial);
@@ -306,21 +334,23 @@ public:
 	}
 };
 
-class ScoreboardButton : public UIButton {
+class ScoreboardButton : public GraphicalGameObject
+{
+private:
+	sf::Texture scoreButtonTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	ScoreboardButton() : UIButton("Scoreboard", { 372.f, 482.f }, { 250.f, 75.f })
+	ScoreboardButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->setFont("Lycanthrope.ttf");
-		this->setTextSize(50.f);
-		this->setTextColor({ 179, 45, 0 });
-		this->setBackgroundColor(sf::Color::White);
-		//this->setBackgroundColor({ 0, 0, 0, 0 });
+		this->scoreButtonTexture.loadFromFile("menu_score.png");
+		this->spritePtr()->setTexture(this->scoreButtonTexture);
+		this->spritePtr()->setPosition(420.f, 540.f);
 	}
 
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			ScoreBoard* scoreBoard = new ScoreBoard();
 			this->screen->addUIObject(scoreBoard);
@@ -328,22 +358,23 @@ public:
 	}
 };
 
-class QuitButton : public UIButton
+class QuitButton : public GraphicalGameObject
 {
+private:
+	sf::Texture quitButtonTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
 public:
-	QuitButton() : UIButton("Escape", { 372.f, 562.f }, { 250.f, 75.f })
+	QuitButton() : GraphicalGameObject(sf::Sprite())
 	{
-		this->setFont("Lycanthrope.ttf");
-		this->setTextSize(50.f);
-		this->setTextColor({ 179, 45, 0 });
-		this->setBackgroundColor(sf::Color::White);
-		//this->setBackgroundColor({ 0, 0, 0, 0 });
+		this->quitButtonTexture.loadFromFile("menu_escape.png");
+		this->spritePtr()->setTexture(this->quitButtonTexture);
+		this->spritePtr()->setPosition(410.f, 630.f);
 	}
 
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (e.mouseButton.button == sf::Mouse::Button::Left
-			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
+			&& this->spritePtr()->getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y)))
 		{
 			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
 			musicPlayer.stop();
