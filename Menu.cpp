baf9a2797cebd92103993459834a548877b3656f
Leia Lin <astrob3rry@gmail.com>
2019-03-13 10:23:19 -0700

    cleaned the code

--- Menu.cpp
+++ Menu.cpp
@@ -1,13 +1,18 @@
 #include "Menu.h"
-#include "TestLevel.h"
-#include "DifficultySettings.h"
 #include "ScoreBoard.h"
 #include "Tutorial.h"
-#include "UIButton.h"
-#include <string>
 
 class PlayerNameEntry : public GraphicalGameObject
 {
+private:
+	std::string name;
+	sf::Font font;
+	sf::Texture texture;
+	sf::Sprite background;
+	sf::Clock clock;
+	bool ready = false;
+	bool decline = false;
+	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->graphic); }
 public:
 	PlayerNameEntry() : GraphicalGameObject(sf::Text())
 	{
@@ -16,7 +21,7 @@ public:
 		this->textPtr()->setStyle(sf::Text::Bold);
 		this->textPtr()->setFillColor(sf::Color(179, 45, 0));
 		this->textPtr()->setCharacterSize(50U);
-		if (DifficultySettings::currentDifficulty != DifficultySettings::TEST)
+		if (DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST)
 		{
 			this->textPtr()->setPosition(150.f, 150.f);
 			this->textPtr()->setString("Enter your name while you can:\n");
@@ -26,112 +31,96 @@ public:
 			this->textPtr()->setPosition(100.f, 80.f);
 			this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n");
 		}
-		background.setPosition(600.f, 400.f);
-		texture.loadFromFile("bloodyhands.png");
-		background.setTexture(texture);
-		ready = false;
-		decline = false;
-		for (auto obj : Menu::getCurrentMenu()->getMenuObjects())
-		{
-			if (obj != this) { obj->disableEvents(); }
-		}
+		this->background.setPosition(600.f, 400.f);
+		this->texture.loadFromFile("bloodyhands.png");
+		this->background.setTexture(texture);
+		for (auto obj : Menu::getCurrentMenu()->getMenuObjects()) { if (obj != this) { obj->disableEvents(); } }
 	}
+
 	void RemovedFromScreen()
 	{
-		for (auto obj : Menu::getCurrentMenu()->getMenuObjects())
-		{
-			if (obj != this) { obj->enableEvents(); }
-		}
+		for (auto obj : Menu::getCurrentMenu()->getMenuObjects()) { if (obj != this) { obj->enableEvents(); } }
 	}
+
 	void draw(sf::RenderWindow& win)
 	{
 		win.clear();
-		win.draw(background);
+		win.draw(this->background);
 		win.draw(*this->textPtr());
 		Music::ID music;
-		if (ready && DifficultySettings::currentDifficulty != DifficultySettings::TEST)
+		if (this->ready && DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST)
 		{
 			win.display();
 			do
 			{
 				;
-			} while (clock.getElapsedTime().asSeconds() < 1.5f); // wait for a second for the good luck text to display before entering the game
+			} while (this->clock.getElapsedTime().asSeconds() < 1.5f); // pause the good luck msg for some time before the real game start
 			{
 				switch (DifficultySettings::currentDifficulty)
 				{
-				case DifficultySettings::EASY:
+				case DifficultySettings::DIFFICULTY::EASY:
 					music = Music::ID::EasyGame;
 					break;
-				case DifficultySettings::NORMAL:
+				case DifficultySettings::DIFFICULTY::NORMAL:
 					music = Music::ID::NormalGame;
 					break;
-				case DifficultySettings::HARD:
+				case DifficultySettings::DIFFICULTY::HARD:
 					music = Music::ID::HardGame;
 					break;
 				default:
 					music = Music::ID::TestMode;
 					break;
 				}
-				Engine::musicPlayer.stop();
-				Engine::musicPlayer.play(music);
-				Engine::musicPlayer.setVolume(20.f);
+				musicPlayer.stop();
+				musicPlayer.play(music);
+				musicPlayer.setVolume(20.f);
 				this->screen->remove(this);
 				Menu::getCurrentMenu()->startTestLevel(this->name);
 			}
 		}
-		
-		if (ready && DifficultySettings::currentDifficulty == DifficultySettings::TEST)
+		if (this->ready && DifficultySettings::currentDifficulty == DifficultySettings::DIFFICULTY::TEST)
 		{
 			music = Music::ID::TestMode;
 			win.display();
 			do
 			{
 				;
-			} while (clock.getElapsedTime().asSeconds() < 1.5f);
-			Engine::musicPlayer.stop();
-			Engine::musicPlayer.play(music);
-			Engine::musicPlayer.setVolume(20.f);
+			} while (this->clock.getElapsedTime().asSeconds() < 1.5f);
+			musicPlayer.stop();
+			musicPlayer.play(music);
+			musicPlayer.setVolume(20.f);
 			this->screen->remove(this);
 			Menu::getCurrentMenu()->startTestLevel(this->name);
 		}
-
-		if (decline)
+		if (this->decline)
 		{
 			win.display();
 			do
 			{
 				;
-			} while (clock.getElapsedTime().asSeconds() < 1.5f);
-			decline = false;
+			} while (this->clock.getElapsedTime().asSeconds() < 1.5f);
+			this->decline = false;
 			this->screen->remove(this);
 		}
 	}
+
 	void TextEntered(sf::Event e)
 	{
 		int key = e.text.unicode;
 		if (key == 8) //8 is delete
 		{
-			if (this->name.size() > 0)
-			{
-				this->name = this->name.substr(0, this->name.size() - 1);
-			}
+			if (this->name.size() > 0) { this->name = this->name.substr(0, this->name.size() - 1); }
 
-			if (DifficultySettings::currentDifficulty != DifficultySettings::TEST)
-			{
-				this->textPtr()->setString("Enter your name while you can:\n" + this->name);
-			}
-			else
-			{
-				this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n" + this->name);
-			}
+			if (DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST) { this->textPtr()->setString("Enter your name while you can:\n" + this->name); }
+			else { this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n" + this->name); }
 		}
 		else if (key == static_cast<int>('\r') || key == static_cast<int>('\n'))
 		{
-			if (DifficultySettings::currentDifficulty != DifficultySettings::TEST)
+			if (DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST)
 			{
 				this->textPtr()->setString("Enter your name while you can:\n" + this->name + "\n\nGood luck, " + this->name + "!");
-				ready = true;
-				clock.restart();
+				this->ready = true;
+				this->clock.restart();
 			}
 			else
 			{
@@ -139,132 +128,111 @@ public:
 				if (this->name != "our game is better than yours! :)")
 				{
 					this->textPtr()->setString("Sorry, get out!!!");
-					decline = true;
-					clock.restart();
+					this->decline = true;
+					this->clock.restart();
 				}
 				else
 				{
 					this->textPtr()->setString("Welcome, my master...");
-					ready = true;
-					clock.restart();
+					this->ready = true;
+					this->clock.restart();
 				}
 			}
 		}
 		else
 		{
 			this->name += static_cast<char>(e.text.unicode);
-			if (DifficultySettings::currentDifficulty != DifficultySettings::TEST)
-			{
-				this->textPtr()->setString("Enter your name while you can:\n" + this->name);
-			}
-			else
-			{
-				this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n" + this->name);
-			}
+			if (DifficultySettings::currentDifficulty != DifficultySettings::DIFFICULTY::TEST) { this->textPtr()->setString("Enter your name while you can:\n" + this->name); }
+			else { this->textPtr()->setString("Oops, you've entered a secret base\n created by your family, but you need \na password to enter:\n" + this->name); }
 		}
 	}
-private:
-	std::string name;
-	sf::Font font;
-	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->graphic); }
-	sf::Texture texture;
-	sf::Sprite background;
-	bool ready;
-	bool decline;
-	sf::Clock clock;
 };
 
 class TestModeButton : public UIButton
 {
+private:
+	bool activated = false;
+	bool enabled = true;
+	bool enterPressed = false;
 public:
 	TestModeButton() : UIButton("?", { 200.f, 25.f }, { 100.f, 100.f })
 	{
-		myFont.loadFromFile("DoubleFeature.ttf");
+		this->myFont.loadFromFile("harting.ttf");
 		this->textPtr()->setFont(myFont);
 		this->textPtr()->setCharacterSize(50);
 		this->textPtr()->setFillColor({ 179, 45, 0 });
 	}
+
 	void KeyReleased(sf::Event e)
 	{
-		if (e.key.code == sf::Keyboard::Z)
+		if (e.key.code == sf::Keyboard::Enter)
 		{
-			ZPressed++;
-			if (ZPressed == 4 && this->enabled)
-			{
-				this->activated = true;
-			}
+			this->enterPressed = true;
+			if (this->enterPressed && this->enabled) { this->activated = true; }
 		}
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
 		if (!this->enabled || !this->activated) { return; }
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
-			DifficultySettings::setDifficulty(DifficultySettings::TEST);
-			Engine::soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
+			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::TEST);
+			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
 			this->screen->addUIObject(new PlayerNameEntry());
 		}
 	}
+
 	void draw(sf::RenderWindow& win)
 	{
 		if (this->activated && this->enabled) { win.draw(*this->graphic); }
 	}
-	void EveryFrame(uint64_t f)
-	{
-		if (f % 60 == 0)
-		{
-			if (this->ZPressed > 0) { this->ZPressed--; }
-		}
-	}
+
 	void enable()
 	{
 		this->enabled = true;
 	}
+
 	void disable()
 	{
 		this->enabled = false;
 	}
-private:
-	bool activated = false;
-	bool enabled = true;
-	int ZPressed = 0;
 };
 
-class GameTitle : public GraphicalGameObject {
-public:
-	GameTitle() : GraphicalGameObject(sf::Text())
-	{
-		font.loadFromFile("DoubleFeature.ttf");
-		this->textPtr()->setFont(font);
-		this->textPtr()->setString("Cursed Zombie");
-		this->textPtr()->setStyle(sf::Text::Bold);
-		this->textPtr()->setFillColor(sf::Color(179, 45, 0));
-		this->textPtr()->setCharacterSize(50);
-		this->textPtr()->setPosition(340.f, 50.f);
-	}
+class MenuBackground : public GraphicalGameObject
+{
 private:
-	sf::Font font;
-	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->getGraphic()); }
+	sf::Texture backgroundTexture;
+	sf::Sprite* spritePtr() { return dynamic_cast<sf::Sprite*>(this->getGraphic()); }
+public:
+	MenuBackground() : GraphicalGameObject(sf::Sprite())
+	{
+		this->backgroundTexture.loadFromFile("menu_background.png");
+		this->spritePtr()->setTexture(backgroundTexture);
+	}
 };
 
 class EasyLevelButton : public UIButton
 {
 public:
-	EasyLevelButton() : UIButton("Easy", { 412.f, 162.f }, { 200.f, 75.f })
+	EasyLevelButton() : UIButton("Easy", { 372.f, 162.f }, { 250.f, 75.f })
 	{
-		this->setFont("DoubleFeature.ttf");
-		this->setTextSize(35.f);
-		this->setTextColor(sf::Color(179, 45, 0));
-		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+		this->setFont("Lycanthrope.ttf");
+		this->setTextSize(50.f);
+		this->setTextColor({ 179, 45, 0 });
+		this->setBackgroundColor(sf::Color::White);
+		//this->setPosition({ 372.f, 162.f });
+		//this->setBackgroundColor({ 0, 0, 0, 0 });
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
-			DifficultySettings::setDifficulty(DifficultySettings::EASY);
-			Engine::soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
+			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::EASY);
+			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
 			this->screen->addUIObject(new PlayerNameEntry());
 		}
 	}
@@ -272,20 +240,22 @@ public:
 
 class NormalLevelButton : public UIButton {
 public:
-	NormalLevelButton() : UIButton("Normal", { 412.f, 242.f }, { 200.f, 75.f })
+	NormalLevelButton() : UIButton("Normal", { 372.f, 242.f }, { 250.f, 75.f })
 	{
-		this->setFont("DoubleFeature.ttf");
-		this->setTextSize(35.f);
-		this->setTextColor(sf::Color(179, 45, 0));
-		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+		this->setFont("Lycanthrope.ttf");
+		this->setTextSize(50.f);
+		this->setTextColor({ 179, 45, 0 });
+		this->setBackgroundColor(sf::Color::White);
+		//this->setBackgroundColor({ 0, 0, 0, 0 });
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
-			DifficultySettings::setDifficulty(DifficultySettings::NORMAL);
-			Engine::soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
+			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::NORMAL);
+			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
 			this->screen->addUIObject(new PlayerNameEntry());
 		}
 	}
@@ -293,20 +263,22 @@ public:
 
 class HardLevelButton : public UIButton {
 public:
-	HardLevelButton() : UIButton("Insane", { 412.f, 322.f }, { 200.f, 75.f })
+	HardLevelButton() : UIButton("Insane", { 372.f, 322.f }, { 250.f, 75.f })
 	{
-		this->setFont("DoubleFeature.ttf");
-		this->setTextSize(35.f);
-		this->setTextColor(sf::Color(179, 45, 0));
-		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+		this->setFont("Lycanthrope.ttf");
+		this->setTextSize(50.f);
+		this->setTextColor({ 179, 45, 0 });
+		this->setBackgroundColor(sf::Color::White);
+		//this->setBackgroundColor({ 0, 0, 0, 0 });
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
-			DifficultySettings::setDifficulty(DifficultySettings::HARD);
-			Engine::soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
+			DifficultySettings::setDifficulty(DifficultySettings::DIFFICULTY::HARD);
+			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
 			this->screen->addUIObject(new PlayerNameEntry());
 		}
 	}
@@ -314,16 +286,18 @@ public:
 
 class TutorialButton : public UIButton {
 public:
-	TutorialButton() : UIButton("Tutorial", { 412.f, 402.f }, { 200.f, 75.f })
+	TutorialButton() : UIButton("Tutorial", { 372.f, 402.f }, { 250.f, 75.f })
 	{
-		this->setFont("DoubleFeature.ttf");
-		this->setTextSize(35.f);
-		this->setTextColor(sf::Color(179, 45, 0));
-		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+		this->setFont("Lycanthrope.ttf");
+		this->setTextSize(50.f);
+		this->setTextColor({ 179, 45, 0 });
+		this->setBackgroundColor(sf::Color::White);
+		//this->setBackgroundColor({ 0, 0, 0, 0 });
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
 			Tutorial* tutorial = new Tutorial();
@@ -334,16 +308,18 @@ public:
 
 class ScoreboardButton : public UIButton {
 public:
-	ScoreboardButton() : UIButton("Scoreboard", { 412.f, 482.f }, { 200.f, 75.f })
+	ScoreboardButton() : UIButton("Scoreboard", { 372.f, 482.f }, { 250.f, 75.f })
 	{
-		this->setFont("DoubleFeature.ttf");
-		this->setTextSize(35.f);
-		this->setTextColor(sf::Color(179, 45, 0));
-		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+		this->setFont("Lycanthrope.ttf");
+		this->setTextSize(50.f);
+		this->setTextColor({ 179, 45, 0 });
+		this->setBackgroundColor(sf::Color::White);
+		//this->setBackgroundColor({ 0, 0, 0, 0 });
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
 			ScoreBoard* scoreBoard = new ScoreBoard();
@@ -355,20 +331,22 @@ public:
 class QuitButton : public UIButton
 {
 public:
-	QuitButton() : UIButton("Escape", { 412.f, 562.f }, { 200.f, 75.f })
+	QuitButton() : UIButton("Escape", { 372.f, 562.f }, { 250.f, 75.f })
 	{
-		this->setFont("DoubleFeature.ttf");
-		this->setTextSize(35.f);
-		this->setTextColor(sf::Color(179, 45, 0));
-		this->setBackgroundColor(sf::Color(0, 0, 0, 0));
+		this->setFont("Lycanthrope.ttf");
+		this->setTextSize(50.f);
+		this->setTextColor({ 179, 45, 0 });
+		this->setBackgroundColor(sf::Color::White);
+		//this->setBackgroundColor({ 0, 0, 0, 0 });
 	}
+
 	void MouseButtonReleased(sf::Event e)
 	{
-		if (e.mouseButton.button == sf::Mouse::Button::Left //if the left mouse button was clicked
+		if (e.mouseButton.button == sf::Mouse::Button::Left
 			&& this->background.getGlobalBounds().contains(static_cast<float>(e.mouseButton.x), static_cast<float>(e.mouseButton.y))) //if the click was inside the button
 		{
-			Engine::soundPlayer.play(SoundEffect::MenuClick, 20.f);
-			Engine::musicPlayer.stop();
+			soundPlayer.play(SoundEffect::ID::MenuClick, 20.f);
+			musicPlayer.stop();
 			this->screen->close();
 		}
 	}
@@ -376,20 +354,18 @@ public:
 
 namespace Engine
 {
-	static Menu* currentMenu = nullptr;
-
 	Menu::Menu()
 	{
 		currentMenu = this;
 		this->menuObjects = {
-			new GameTitle(),
+			new MenuBackground(),
 			new EasyLevelButton(),
 			new NormalLevelButton(),
 			new HardLevelButton(),
 			new TutorialButton(),
 			new ScoreboardButton(),
 			new QuitButton(),
-			new TestModeButton()
+			new TestModeButton(),
 		};
 	}
 
@@ -404,11 +380,8 @@ namespace Engine
 
 	void Menu::start()
 	{
-		Engine::musicPlayer.play(Music::Menu);
-		for (auto obj : this->menuObjects)
-		{
-			menuScreen.addUIObject(obj);
-		}
+		musicPlayer.play(Music::ID::Menu);
+		for (auto obj : this->menuObjects) { this->menuScreen.addUIObject(obj); }
 		this->menuScreen.render();
 	}
 
