f9cf71f502965f8e9251a44e62ce748722ca5630
Leia Lin <astrob3rry@gmail.com>
2019-03-13 10:24:55 -0700

    cleaned the code

--- SampleUIText.h
+++ SampleUIText.h
@@ -10,45 +10,49 @@ class SampleUIText : public GraphicalGameObject
 public:
 	SampleUIText(std::string text) : GraphicalGameObject(sf::Text())
 	{
-		if (myFont.loadFromFile("arial.ttf"))
-		{			
+		if (myFont.loadFromFile("zombie.ttf"))
+		{
 			this->textPtr()->setFont(myFont);
 			this->textPtr()->setString(text);
 			this->textPtr()->setPosition(0, 0);
 		}
 	}
-    SampleUIText(std::string text, const sf::Color &C, unsigned int size,
-                 float position_x, float position_y) :  GraphicalGameObject(sf::Text()){
-        if (myFont.loadFromFile("arial.ttf"))
-        {
-            this->textPtr()->setFont(myFont);
-            this->textPtr()->setString(text);
-            this->textPtr()->setPosition(position_x, position_y);
-            this->textPtr()->setFillColor(C);
-            this->textPtr()->setCharacterSize(size);
-        }
-        
-    }
-    
-    SampleUIText(std::string filename, std::string text, const sf::Color &C, unsigned int size,
-                 float position_x, float position_y) :  GraphicalGameObject(sf::Text()){
-        if (myFont.loadFromFile(filename))
-        {
-            this->textPtr()->setFont(myFont);
-            this->textPtr()->setString(text);
-            this->textPtr()->setPosition(position_x, position_y);
-            this->textPtr()->setFillColor(C);
-            this->textPtr()->setCharacterSize(size);
-        }
-        
-    }
-    
-    void setText(std::string text){
-        this->textPtr()->setString(text);
-    }
-    void setPosition(float position_x, float position_y){
-        this->textPtr()->setPosition(position_x, position_y);
-    }
+	SampleUIText(std::string text, const sf::Color &C, unsigned int size,
+		float position_x, float position_y) : GraphicalGameObject(sf::Text()) {
+		if (myFont.loadFromFile("zombie.ttf"))
+		{
+			this->textPtr()->setFont(myFont);
+			this->textPtr()->setOutlineThickness(2.f);
+			this->textPtr()->Bold;
+			this->textPtr()->setOutlineColor(sf::Color(179, 45, 0));
+			this->textPtr()->setLetterSpacing(3.f);
+			this->textPtr()->setString(text);
+			this->textPtr()->setPosition(position_x, position_y);
+			this->textPtr()->setFillColor(C);
+			this->textPtr()->setCharacterSize(size);
+		}
+
+	}
+
+	SampleUIText(std::string filename, std::string text, const sf::Color &C, unsigned int size,
+		float position_x, float position_y) : GraphicalGameObject(sf::Text()) {
+		if (myFont.loadFromFile(filename))
+		{
+			this->textPtr()->setFont(myFont);
+			this->textPtr()->setString(text);
+			this->textPtr()->setPosition(position_x, position_y);
+			this->textPtr()->setFillColor(C);
+			this->textPtr()->setCharacterSize(size);
+		}
+
+	}
+
+	void setText(std::string text) {
+		this->textPtr()->setString(text);
+	}
+	void setPosition(float position_x, float position_y) {
+		this->textPtr()->setPosition(position_x, position_y);
+	}
 protected:
 	sf::Font myFont;
 	sf::Text* textPtr() { return dynamic_cast<sf::Text*>(this->getGraphic()); }
