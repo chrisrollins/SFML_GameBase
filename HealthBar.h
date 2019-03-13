018ec162785e6a93bfd14ed80c3ed451ffd15081
Leia Lin <astrob3rry@gmail.com>
2019-03-13 10:22:06 -0700

    cleaned the code

--- HealthBar.h
+++ HealthBar.h
@@ -1,24 +1,30 @@
-#ifndef HealthBar_h
-#define HealthBar_h
+#ifndef HEALTHBAR_H
+#define HEALTHBAR_H
 
 #include "MainCharacter.h"
 
 class HealthBar : public GraphicalGameObject
 {
+private:
+	sf::RectangleShape* maxPtr() { return &this->maxHealthBar; } //max health
+	sf::RectangleShape* currPtr() { return dynamic_cast<sf::RectangleShape*>(this->getGraphic()); }	//current health
+	MainCharacter * character = nullptr;
+	sf::RectangleShape maxHealthBar;
+	sf::Clock alarmClock;
+	bool isAlarming = false;
 public:
-	HealthBar() : GraphicalGameObject(sf::RectangleShape(sf::Vector2f(360, 10)))
+	HealthBar() : GraphicalGameObject(sf::RectangleShape({ 360.f, 10.f }))
 	{
-		this->maxPtr()->setSize(sf::Vector2f(360, 15));
-		this->maxPtr()->setPosition(50, 50);
-		this->maxPtr()->setFillColor(sf::Color(255, 255, 255, 140)); //the last number in the sf::Color constructor is alpha (transparency). if it is less than 255 the object will be transparent.
-		this->maxPtr()->setOutlineThickness(3);
-		this->maxPtr()->setOutlineColor(sf::Color(0, 0, 0, 100));
+		this->maxPtr()->setSize({ 360.f, 15.f });
+		this->maxPtr()->setPosition(50.f, 50.f);
+		this->maxPtr()->setFillColor({ 255, 255, 255, 140 });
+		this->maxPtr()->setOutlineColor({ 0, 0, 0, 100 });
 		this->currPtr()->setOutlineThickness(3);
-		this->currPtr()->setOutlineColor(sf::Color(0, 0, 0, 100));
-		isAlarming = false;
+		this->currPtr()->setOutlineColor({ 0, 0, 0, 100 });
 	}
 
-	void setCharacter(MainCharacter * mc) {
+	void setCharacter(MainCharacter * mc) 
+	{
 		this->character = mc;
 		float fHealth = static_cast<float>(this->character->getHealth());
 		this->maxPtr()->setSize(sf::Vector2f(fHealth / 600.f, 10.f));
@@ -27,42 +33,29 @@ public:
 		this->currPtr()->setFillColor(sf::Color(sf::Color::Green));
 	}
 
-	void EveryFrame(uint64_t f) {
+	void EveryFrame(uint64_t f) 
+	{
 		float fMaxHealth = static_cast<float>(this->character->getMaxHealth());
 		float fCurrHealth = static_cast<float>(this->character->getHealth());
-		if (this->character->getHealth() > this->character->getMaxHealth())
-		{
-			this->currPtr()->setSize(sf::Vector2f(fMaxHealth / 600.f, 10.f));
-		}
-		else
-		{
-			this->currPtr()->setSize(sf::Vector2f(fCurrHealth / 600.f, 10.f));
-		}
 
-		if (this->character->getHealth() > (this->character->getMaxHealth() / 2))
-		{
-			this->currPtr()->setFillColor(sf::Color(sf::Color::Green));
-		}
-		else if (this->character->getHealth() > (this->character->getMaxHealth() / 4))
-		{
-			this->currPtr()->setFillColor(sf::Color(sf::Color::Yellow));
-		}
+		if (this->character->getHealth() > this->character->getMaxHealth()) { this->currPtr()->setSize({ fMaxHealth / 600.f, 10.f }); }
+		else { this->currPtr()->setSize({ fCurrHealth / 600.f, 10.f }); }
+
+		if (this->character->getHealth() > (this->character->getMaxHealth() / 2)) { this->currPtr()->setFillColor(sf::Color::Green); }
+		else if (this->character->getHealth() > (this->character->getMaxHealth() / 4)){ this->currPtr()->setFillColor(sf::Color::Yellow); }
 		else
 		{
-			if (character->getHealth() > 0)
+			if (this->character->getHealth() > 0)
 			{
-				if (!isAlarming)
+				if (!this->isAlarming)
 				{
-					isAlarming = true;
-					alarmClock.restart();
+					this->isAlarming = true;
+					this->alarmClock.restart();
 					Engine::soundPlayer.play(SoundEffect::ID::Alarm, 20.f);
 				}
-				else if (alarmClock.getElapsedTime().asSeconds() > 3.5)
-				{
-					isAlarming = false;
-				}
+				else if (this->alarmClock.getElapsedTime().asSeconds() > 3.5) { this->isAlarming = false; }
 			}
-			this->currPtr()->setFillColor(sf::Color(sf::Color::Red));
+			this->currPtr()->setFillColor(sf::Color::Red);
 		}
 	}
 
@@ -72,16 +65,6 @@ public:
 		win.draw(*this->maxPtr());
 		win.draw(*this->currPtr());
 	}
-
-private:
-	//max health
-	sf::RectangleShape* maxPtr() { return &this->maxHealthBar; }
-	//current health
-	sf::RectangleShape* currPtr() { return dynamic_cast<sf::RectangleShape*>(this->getGraphic()); }
-	MainCharacter * character = nullptr;
-	sf::RectangleShape maxHealthBar;
-	sf::Clock alarmClock;
-	bool isAlarming;
 };
 
-#endif /* HealthBar_h */
+#endif
