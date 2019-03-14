b13bbfbb4227f2ebf1a1b2168750741f3539c66f
Leia Lin <astrob3rry@gmail.com>
2019-03-13 20:27:54 -0700

    Update TestLevel.h

--- TestLevel.h
+++ TestLevel.h
@@ -24,84 +24,83 @@ public:
 		if (!map.load(DifficultySettings::Map::picture, DifficultySettings::Map::fileName)) { throw GameException::ImageFileLoadException(DifficultySettings::Map::fileName); }
 		levelScreen->addMap(&map);
 
-		sf::Sprite m_Sprite;
-		m_Sprite.setPosition(static_cast<float>(map.width() * map.tileSize().x / 2),
+		static sf::Texture mTexture;
+		if (!mTexture.loadFromFile("zombie.png")) { throw GameException::ImageFileLoadException("zombie.png"); }
+		sf::Sprite mCharacter;
+		mCharacter.setTexture(mTexture);
+		mCharacter.setPosition(static_cast<float>(map.width() * map.tileSize().x / 2),
 			static_cast<float>(map.height() * map.tileSize().y / 2));
-		static sf::Texture m_Texture;
-		if (!m_Texture.loadFromFile("zombie.png")) { throw GameException::ImageFileLoadException("zombie.png"); }
-		m_Sprite.setTexture(m_Texture);
+		MainCharacter* mcPtr = new MainCharacter(mCharacter, playerName);
+		levelScreen->addMainCharacter(mcPtr);
 
-		MainCharacter* mc_ptr = new MainCharacter(m_Sprite, playerName);
-		levelScreen->addMainCharacter(mc_ptr);
-
-		static sf::Texture potionUI_texture;
-		if (!potionUI_texture.loadFromFile("brain_icon.png")) { throw GameException::ImageFileLoadException("brain_icon.png"); }
-		sf::Sprite potion_icon;
-		potion_icon.setTexture(potionUI_texture);
-		static PotionUI potionUI(potion_icon);
-		potionUI.setCharacter(mc_ptr);
+		static sf::Texture potionUITexture;
+		if (!potionUITexture.loadFromFile("brain_icon.png")) { throw GameException::ImageFileLoadException("brain_icon.png"); }
+		sf::Sprite potionIcon;
+		potionIcon.setTexture(potionUITexture);
+		static PotionUI potionUI(potionIcon);
+		potionUI.setCharacter(mcPtr);
 		levelScreen->addUIObject(&potionUI);
 
-		static sf::Texture citizen_boy_texture;
-		if (!citizen_boy_texture.loadFromFile("boy.png")) { throw GameException::ImageFileLoadException("boy.png"); }
+		static sf::Texture boyTexture;
+		if (!boyTexture.loadFromFile("boy.png")) { throw GameException::ImageFileLoadException("boy.png"); }
 		sf::Sprite boy;
-		boy.setTexture(citizen_boy_texture);
+		boy.setTexture(boyTexture);
 		// 5: the max number
 		// 200: respawn this object per 200 frames
 		static RespawnManager<Citizen> boyMng(boy, 5, 200);
 		boyMng.clear();
 		levelScreen->add(&boyMng);
 
-		static sf::Texture citizen_girl_texture;
-		if (!citizen_girl_texture.loadFromFile("girl.png")) { throw GameException::ImageFileLoadException("girl.png"); }
+		static sf::Texture girlTexture;
+		if (!girlTexture.loadFromFile("girl.png")) { throw GameException::ImageFileLoadException("girl.png"); }
 		sf::Sprite girl;
-		girl.setTexture(citizen_girl_texture);
+		girl.setTexture(girlTexture);
 		static RespawnManager<Citizen> girlMng(girl, 3, 200);
 		girlMng.clear();
 		levelScreen->add(&girlMng);
 
-		static sf::Texture citizen_man_texture;
-		if (!citizen_man_texture.loadFromFile("man.png")) { throw GameException::ImageFileLoadException("man.png"); }
+		static sf::Texture manTexture;
+		if (!manTexture.loadFromFile("man.png")) { throw GameException::ImageFileLoadException("man.png"); }
 		sf::Sprite man;
-		man.setTexture(citizen_man_texture);
+		man.setTexture(manTexture);
 		static RespawnManager<Citizen> manMng(man, 3, 200);
 		manMng.clear();
 		levelScreen->add(&manMng);
 
-		static sf::Texture citizen_woman_texture;
-		if (!citizen_woman_texture.loadFromFile("woman.png")) { throw GameException::ImageFileLoadException("woman.png"); }
+		static sf::Texture womanTexture;
+		if (!womanTexture.loadFromFile("woman.png")) { throw GameException::ImageFileLoadException("woman.png"); }
 		sf::Sprite woman;
-		woman.setTexture(citizen_woman_texture);
+		woman.setTexture(womanTexture);
 		static RespawnManager<Citizen> womanMng(woman, 3, 200);
 		womanMng.clear();
 		levelScreen->add(&womanMng);
 
-		static sf::Texture citizen_oldman_texture;
-		if (!citizen_oldman_texture.loadFromFile("oldman.png")) { throw GameException::ImageFileLoadException("oldman.png"); }
+		static sf::Texture oldmanTexture;
+		if (!oldmanTexture.loadFromFile("oldman.png")) { throw GameException::ImageFileLoadException("oldman.png"); }
 		sf::Sprite oldman;
-		oldman.setTexture(citizen_oldman_texture);
+		oldman.setTexture(oldmanTexture);
 		static RespawnManager<Citizen> oldmanMng(oldman, 3, 200);
 		oldmanMng.clear();
 		levelScreen->add(&oldmanMng);
 
-		static sf::Texture citizen_oldwoman_texture;
-		if (!citizen_oldwoman_texture.loadFromFile("oldwoman.png")) { throw GameException::ImageFileLoadException("oldwoman.png"); }
+		static sf::Texture oldwomanTexture;
+		if (!oldwomanTexture.loadFromFile("oldwoman.png")) { throw GameException::ImageFileLoadException("oldwoman.png"); }
 		sf::Sprite oldwoman;
-		oldwoman.setTexture(citizen_oldwoman_texture);
+		oldwoman.setTexture(oldwomanTexture);
 		static RespawnManager<Citizen> oldwomanMng(oldwoman, 3, 200);
 		oldwomanMng.clear();
 		levelScreen->add(&oldwomanMng);
 
-		static sf::Texture mage_texture;
-		if (!mage_texture.loadFromFile("mage.png")) { throw GameException::ImageFileLoadException("mage.png"); }
+		static sf::Texture mageTexture;
+		if (!mageTexture.loadFromFile("mage.png")) { throw GameException::ImageFileLoadException("mage.png"); }
 		sf::Sprite mage;
-		mage.setTexture(mage_texture);
+		mage.setTexture(mageTexture);
 		static RespawnManager<Mage> soldierMng(mage, 12, 175);
 		soldierMng.clear();
 		levelScreen->add(&soldierMng);
 
 		static HealthBar healthbar;
-		healthbar.setCharacter(mc_ptr);
+		healthbar.setCharacter(mcPtr);
 		levelScreen->addUIObject(&healthbar);
 
 		//set up the score object
@@ -114,7 +113,7 @@ public:
 
 		static sf::Text t;
 		static TimerUI timer(t);
-		timer.setCharacter(mc_ptr);
+		timer.setCharacter(mcPtr);
 		levelScreen->addUIObject(&timer);
 
 		if (oldScreen)
